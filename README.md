540612452356ddcb575cce7c75d5faf00644abf3
Ralph Cowling <acastofthousands@gmail.com>
2013-04-18 10:02:24 +0100

    add script and example image

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-bashmoji
-========
+💪💪Bashmoji💪💪
+=================
 
-Emojis in your prompt let you know what time it is.
+Displays an emjoi based on the time of day (OS X).
+
+Borrows heavily from the very fun https://github.com/paulsm/bashtime
+
+Include it in your PS1 variable bruv
+
+    export PS1="\$(/PATH/TO/bashmoji.sh) \w "
+
+Will look something like:
+
+![Bashmoji](example.jpg)
---
+++ bashmoji.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+
+# bashmoji.sh
+# By Ralph Cowling
+# This is a silly fork of a good thing by Paul Scott-Murphy see https://github.com/paulsm/bashtime for more
+
+
+time=`date +%H%M`; #24hr time str
+if [ "$time" -lt 0700 ]
+then
+    echo -n '🌄'
+elif [ "$time" -lt 0900 ]
+then
+    echo -n '🌅'
+elif [ "$time" -lt 1300 ]
+then
+    echo -n '🌞'
+elif [ "$time" -lt 1700 ]
+then
+    echo -n '🌈'
+elif [ "$time" -lt 1930 ]
+then
+    echo -n '🌆'
+elif [ "$time" -lt 2100 ]
+then
+    echo -n '🌃'
+elif [ "$time" -lt 2300 ]
+then
+    echo -n '🌠'
+elif [ "$time" -lt 0300 ]
+then
+    echo -n '🌝'
+elif [ "$time" -lt 0600 ]
+then
+    echo -n '🌜'
+else
+    echo -n '💪'
+fi
